a828e11c17e857cf27ba67feb9f8cecd47bbee4c
smithbek <danielmueller12222@gmail.com>
2025-07-06 13:04:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.BOT_TOKEN;
-const WEBAPP_URL = 'https://link.winna.com/tg-webapp';
+const WEBAPP_URL = 'https://winna.com/?utm_source=telegram&utm_medium=telegramappbot';
 const IMAGE_URL = 'https://winna.fra1.cdn.digitaloceanspaces.com/casino_promotions/telegram%20bot%20banner/Reddit%20-%201920x384%20(2).png';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
