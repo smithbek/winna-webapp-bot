e409123703841defbf2eb80ec8445a6deafac112
smithbek <danielmueller12222@gmail.com>
2025-07-06 12:57:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.BOT_TOKEN;
 const WEBAPP_URL = 'https://link.winna.com/tg-webapp';
-const IMAGE_URL = 'https://winna.fra1.cdn.digitaloceanspaces.com/casino_promotions/telegram%20bot%20banner/Reddit%20-%201920x384%20(2).png'; //
+const IMAGE_URL = 'https://winna.fra1.cdn.digitaloceanspaces.com/casino_promotions/telegram%20bot%20banner/Reddit%20-%201920x384%20(2).png';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
@@ -19,5 +19,4 @@ bot.onText(/\/start/, (msg) => {
       ]]
     }
   });
-});
 });
